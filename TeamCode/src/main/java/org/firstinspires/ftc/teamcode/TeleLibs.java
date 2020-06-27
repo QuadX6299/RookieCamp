e7b4edc7d5f5a1395358b8fffe00aa998ec28e11
luquibotbol <luquibotbol@gmail.com>
2020-06-27 18:52:02 -0500

    hoala

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleLibs.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public abstract class TeleLibs extends OpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor middleLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor middleRight;
+    private DcMotor backRight;
+
+    private DcMotor intakeL;
+    private DcMotor intakeR;
+
+    @Override
+    public void init() {
+        // MOTOR INITIALZATION
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        middleLeft = hardwareMap.dcMotor.get("middleLeft");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
+        intakeL = hardwareMap.dcMotor.get("intakeL");
+        intakeR = hardwareMap.dcMotor.get("intakeR");
+    
