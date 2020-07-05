a776914003c63343c3bd3b81f80b7bc43aed1486
luquibotbol <luquibotbol@gmail.com>
2020-07-05 16:33:46 -0500

    Update TeleLibs.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleLibs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleLibs.java
@@ -8,8 +8,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public abstract class TeleLibs extends OpMode {
 
     private DcMotor frontLeft;
-    private DcMotor frontRight;
     private DcMotor backLeft;
+    private DcMotor frontRight;
     private DcMotor backRight;
 
     private DcMotor intakeL;
@@ -25,4 +25,57 @@ public abstract class TeleLibs extends OpMode {
 
         intakeL = hardwareMap.dcMotor.get("intakeL");
         intakeR = hardwareMap.dcMotor.get("intakeR");
-    
+        
+    // ________________________________________________________________________________________________
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intakeL.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+
+        // =======================================  DRIVE  =============================================
+
+    public void arcadeDrive() {
+        //checking for valid range to apply power (has to give greater power than .1)
+        if (((Math.abs(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y))) > .1) ||
+                Math.abs(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4) > .1) {
+
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double theta = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = -gamepad1.right_stick_x;
+
+            //as per unit circle cos gives x, sin gives you y
+            double FL = r * Math.cos(theta) + rightX;
+            double FR = r * Math.sin(theta) - rightX;
+            double BL = r * Math.sin(theta) + rightX;
+            double BR = r * Math.cos(theta) - rightX;
+
+            //make sure you don't try and give power bigger than 1
+            if (((Math.abs(FL) > 1) || (Math.abs(BL) > 1)) || ((Math.abs(FR) > 1) || (Math.abs(BR) > 1))) {
+                FL /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
+                BL /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
+                FR /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
+                BR /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
+
+            }
+            frontLeft.setPower(FL);
+            backLeft.setPower(FR);
+            frontRight.setPower(BL);
+            backRight.setPower(BR);
+
+        }
+        else {
+            fl.setPower(0);
+            fr.setPower(0);
+            bl.setPower(0);
+            br.setPower(0);
+
+        }
+
+    }
+    // https://github.com/rohitchawla28/LactoseIntolerant_/blob/master/ftc_app-master/TeamCode/src/main/java/LactoseIntolerant/Motion.java
