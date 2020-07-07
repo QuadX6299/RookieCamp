18d1d51164a14b9eb8df3b8489a332d101eb5e86
luquibotbol <luquibotbol@gmail.com>
2020-07-07 15:50:47 -0500

    Update TeleLibs.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleLibs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleLibs.java
@@ -34,48 +34,40 @@ public abstract class TeleLibs extends OpMode {
 
         intakeL.setDirection(DcMotorSimple.Direction.FORWARD);
         intakeR.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
 
 
 
         // =======================================  DRIVE  =============================================
-
+   
     public void arcadeDrive() {
         //checking for valid range to apply power (has to give greater power than .1)
-        if (((Math.abs(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y))) > .1) ||
-                Math.abs(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4) > .1) {
+        if ((Math.abs(gamepad1.left_stick_y) > .1) ||
+                Math.abs(gamepad1.right_stick_x) > .1) {
 
-            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double theta = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = -gamepad1.right_stick_x;
-
-            //as per unit circle cos gives x, sin gives you y
-            double FL = r * Math.cos(theta) + rightX;
-            double FR = r * Math.sin(theta) - rightX;
-            double BL = r * Math.sin(theta) + rightX;
-            double BR = r * Math.cos(theta) - rightX;
-
-            //make sure you don't try and give power bigger than 1
-            if (((Math.abs(FL) > 1) || (Math.abs(BL) > 1)) || ((Math.abs(FR) > 1) || (Math.abs(BR) > 1))) {
-                FL /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
-                BL /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
-                FR /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
-                BR /= Math.max(Math.max(Math.abs(FL), Math.abs(FR)), Math.max(Math.abs(BL), Math.abs(BR)));
-
+            if (Math.abs(left_stick_y > .1)){
+                frontLeft.setPower(-gamepad1.left_stick_y);
+                frontRight.setPower(-gamepad1.left_stick_y);
+                backLeft.setPower(-gamepad1.left_stick_y);
+                backRight.setPower(-gamepad1.left_stick_y); 
             }
-            frontLeft.setPower(FL);
-            backLeft.setPower(FR);
-            frontRight.setPower(BL);
-            backRight.setPower(BR);
+            else {
+                frontLeft.setPower(gamepad1.right_stick_x);
+                frontRight.setPower(-gamepad1.right_stick_x);
+                backLeft.setPower(gamepad1.right_stick_x);
+                backRight.setPower(-gamepad1.right_stick_x);
+            }
 
         }
         else {
-            fl.setPower(0);
-            fr.setPower(0);
-            bl.setPower(0);
-            br.setPower(0);
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
 
         }
 
+    }
     }
     // https://github.com/rohitchawla28/LactoseIntolerant_/blob/master/ftc_app-master/TeamCode/src/main/java/LactoseIntolerant/Motion.java
