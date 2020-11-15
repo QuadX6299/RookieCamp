f5f52c46b92c7a74dcb790750bbc50ce5cfa15c6
anjaligorti <54911406+anjaligorti@users.noreply.github.com>
2020-11-15 01:59:23 -0600

    Update TeleOpTest.java

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -54,137 +54,14 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp", group="Iterative Opmode")
 
-public class TeleOpTest extends OpMode
-{
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor fl;
-    private DcMotor fr;
-    private DcMotor bl;
-    private DcMotor br;
-    private DcMotor vacuum;
-    private DcMotor vacuum2;
-    private Servo wobble;
-    private Servo claw;
-    private output shoot;
-    private transition intake;
-    private boolean shootSwitch;
-
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        fl = hardwareMap.dcMotor.get("fl");
-        fr = hardwareMap.dcMotor.get("fr");
-        bl = hardwareMap.dcMotor.get("bl");
-        br = hardwareMap.dcMotor.get("br");
-        wobble = hardwareMap.servo.get("wobble");
-        claw = hardwareMap.servo.get("claw");
-        vacuum = hardwareMap.dcMotor.get("intake");
-        vacuum2 = hardwareMap.dcMotor.get("intake2");
-
-        shoot = new output(hardwareMap);
-        intake = new transition(hardwareMap);
-
-        //actuator = hardwareMap.dcMotor.get("actuator");
-        //output = hardwareMap.dcMotor.get("output");
-
-        fl.setDirection(DcMotor.Direction.FORWARD);
-        fr.setDirection(DcMotor.Direction.FORWARD);
-        bl.setDirection(DcMotor.Direction.FORWARD);
-        br.setDirection(DcMotor.Direction.FORWARD);
-        vacuum.setDirection(DcMotor.Direction.FORWARD);
-        vacuum2.setDirection(DcMotor.Direction.REVERSE);
-        // Tell the driver that initialization is complete.
-        telemetry.addData("Status", "Initialized");
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-        runtime.reset();
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+public class TeleOpTest extends TeleLibs {
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
-
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
-
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn = gamepad1.right_stick_x;
-        leftPower = Range.clip(drive + turn, -1.0, 1.0);
-        rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
-        // Send calculated power to wheels
-        fl.setPower(leftPower);
-        bl.setPower(leftPower);
-        fr.setPower(rightPower);
-        br.setPower(rightPower);
-
-        shoot.wheelShoot.setPower(gamepad1.right_trigger);
-
-        if(gamepad1.a) {
-            intake.intake.setPower(0.5);
-        } else if(gamepad1.b){
-            intake.intake.setPower(0.6);
-        } else if(gamepad1.x){
-            intake.intake.setPower(0.73);
-        } else if(gamepad1.y){
-            intake.intake.setPower(0.72);
-        } else{
-            intake.intake.setPower(0);
-        }
-
-        vacuum.setPower(gamepad1.left_trigger);
-        vacuum2.setPower(gamepad1.left_trigger);
-
-        if(gamepad1.dpad_down) {
-            wobble.setPosition(0);
-
-        }else if(gamepad1.dpad_up){
-            wobble.setPosition(1);
-
-        }
-
-        if(gamepad1.dpad_left) {
-            claw.setPosition(0);
-        } else if(gamepad1.dpad_right){
-            claw.setPosition(1);
-        }
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
+       Drive();
+       shooter();
+       intake();
+       transition();
+       wobble();
 
     }
 
